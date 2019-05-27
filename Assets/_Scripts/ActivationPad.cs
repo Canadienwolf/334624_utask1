c6b6b4f537c6c0501903b1b88417b3b56de810ce
canadienwolf@gmail.com <canadienwolf@gmail.com>
2019-05-27 22:20:51 +0200

    First Commit

---
+++ Assets/_Scripts/ActivationPad.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivationPad : MonoBehaviour
+{
+
+    public GameObject door;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //OnTriggerEnter is called when an object enters the trigger zone
+    private void OnTriggerEnter()
+    {
+
+    }
+
+    //OnTriggerEnter is called when an object leaves the trigger zone
+    private void OnTriggerExit()
+    {
+
+    }
+}
